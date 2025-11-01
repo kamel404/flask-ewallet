0be2f1a6573a23730439697bb405883a70d06c56
kamel404 <10121641@mu.edu.lb>
2025-11-02 00:33:17 +0200

    tests passed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 import os
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from dotenv import load_dotenv
 
 load_dotenv()
 
